30c2339acd28febb033024c2ef2402d525657971
lehcun <lehcun1099@gmail.com>
2025-09-06 17:39:47 +0700

    Refactor BannerSlider styling

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -9,47 +9,49 @@ import Image from 'next/image';
 export default function BannerSlider() {
   return (
     <div className="bg-linear-to-br from-red-400 to-blue-400">
-      <div className="mx-auto flex max-w-7xl gap-64 py-8 text-white">
-        <div className="flex flex-col gap-4">
-          <h1 className="text-5xl">Công nghệ Tương lai</h1>
-          <p>
-            Khám phá những sản phẩm công nghệ tiên tiến nhất với giá cả cạnh
-            tranh nhất thị trường
-          </p>
+      <div className="mx-4">
+        <div className="mx-auto grid max-w-7xl grid-cols-1 space-y-8 py-8 text-white lg:grid-cols-2">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-5xl">Công nghệ Tương lai</h1>
+            <p>
+              Khám phá những sản phẩm công nghệ tiên tiến nhất với giá cả cạnh
+              tranh nhất thị trường
+            </p>
+          </div>
+          <Swiper
+            modules={[Autoplay, Pagination, Navigation]}
+            autoplay={{ delay: 3000, disableOnInteraction: false }}
+            pagination={{ clickable: true }}
+            navigation
+            loop
+            className="h-64 w-xl rounded-2xl"
+          >
+            <SwiperSlide>
+              <Image
+                src="/banner/banner1.jpg"
+                fill
+                alt="banner1"
+                className="object-cover"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image
+                src="/banner/banner2.jpg"
+                fill
+                alt="banner2"
+                className="object-cover"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image
+                src="/banner/banner3.jpg"
+                fill
+                alt="banner3"
+                className="object-cover"
+              />
+            </SwiperSlide>
+          </Swiper>
         </div>
-        <Swiper
-          modules={[Autoplay, Pagination, Navigation]}
-          autoplay={{ delay: 3000, disableOnInteraction: false }}
-          pagination={{ clickable: true }}
-          navigation
-          loop
-          className="h-64 max-w-lg rounded-2xl"
-        >
-          <SwiperSlide>
-            <Image
-              src="/banner/banner1.jpg"
-              fill
-              alt="banner1"
-              className="object-cover"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image
-              src="/banner/banner2.jpg"
-              fill
-              alt="banner2"
-              className="object-cover"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image
-              src="/banner/banner3.jpg"
-              fill
-              alt="banner3"
-              className="object-cover"
-            />
-          </SwiperSlide>
-        </Swiper>
       </div>
     </div>
   );
