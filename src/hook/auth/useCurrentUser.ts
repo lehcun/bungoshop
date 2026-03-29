fcdfebde0df6917cc03c8b37c7c43392410144f6
lehcun <lehcun1099@gmail.com>
2026-03-29 09:44:49 +0700

    feat: replace fetch with axios in useCurrentUser hook.  update package to include axios dependency

--- src/hook/auth/useCurrentUser.ts
+++ src/hook/auth/useCurrentUser.ts
@@ -1,14 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 export const useCurrentUser = () => {
   const query = useQuery({
     queryKey: ['user'],
     queryFn: async () => {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/me`, {
-        credentials: 'include',
-      });
-      if (!res.ok) throw new Error('Failed to fetch Current user ');
-      return res.json();
+      // Dùng axios thay cho fetch, cực kỳ ổn định với Cookie
+      const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/users/me`,
+        {
+          withCredentials: true, // <--- Lệnh bài thần thánh bắt buộc phải mang Cookie đi
+        }
+      );
+
+      // Không cần check res.ok hay res.json() vì axios đã tự làm và tự quăng lỗi nếu tạch
+      return res.data;
     },
     retry: false, // không cố gọi lại
     refetchOnWindowFocus: false, // Chuyển tab qua lại không tự gọi API nữa
